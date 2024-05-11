fc5b2ea499c12ddb206a56b9e4ea50bc1bca33f9
MuradYagyaev <165575932+MuradYagyaev@users.noreply.github.com>
2024-05-11 17:01:18 +0300

    Delete attributes_and_methods.py

--- attributes_and_methods.py
+++
@@ -1,9 +0,0 @@
-# Модуль №5. Классы и объекты. "Атрибуты и методы объекта."
-class House:
-    def __init__(self):
-        self.numberOfFloors = 10
-
-
-my_house = House()
-for i in range(1, my_house.numberOfFloors + 1):
-    print("Текущий этаж равен", i)
